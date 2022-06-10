3e699394d89167ad211d95e542fbf58cf893665c
RYW <rfaye0313@qq.com>
2022-06-10 18:57:41 +0800

    test

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-//获取元素
+const {Query, User} = AV;
+AV.init({
+    appId: "szRqJxj4rGr47DBsfiYqh9qA-gzGzoHsz",
+    appKey: "UCGehmC6gOKYHSKpoMLeaRFJ",
+    serverURL: "https://szrqjxj4.lc-cn-n1-shared.com"
+});
+
 const sear = document.querySelector("#sear")
 const dothings = document.querySelector(".dothings")
 const donethings = document.querySelector(".donethings")
@@ -9,37 +15,38 @@ load()
 sear.addEventListener("keydown", event => {
     if (event.keyCode === 13) {
         if (sear.value !== '') {
-            // 获取getData()函数里的返回值 一开始返回[] 后面数组里有对象之后 返回对象 JSON.parse
-            let local = getData()
-            //信息以对象的形式储存
-            local.push({ title: sear.value, done: false })
-            //保存数据
-            saveData(local)
-            //清空搜索框里的东西
+            saveData({title: sear.value, done: false})
             sear.value = ''
-            //渲染页面
             load()
         }
     }
 })
 
-function getData() {
-    //获取key为todolist里的字符串 存储在本地里的都是以字符串形式存储的 所以返回的时候要用JSON把data转换为对象形式
-    let data = localStorage.getItem("todolist")
-    if (data != null) {
-        return JSON.parse(data)
-    } else {
-        return []
-    }
+async function getData() {
+    let data = []
+    const queryAll = new AV.Query('Todolist');
+    await queryAll.find().then((rows) => {
+        for (let row of rows) {
+            data.push(row);
+        }
+    });
+    return data
 }
 
 function saveData(data) {
-    //保存data到todolist里 转换为字符串形式
-    return localStorage.setItem("todolist", JSON.stringify(data))
+    const Todo = AV.Object.extend('Todolist');
+    const todo = new Todo();
+    todo.set('title', data.title);
+    todo.set('done', data.done);
+    todo.save().then((todo) => {
+        console.log(`保存成功。objectId：${todo.id}`);
+    }, (error) => {
+        console.log(`保存失败。objectId：${todo.id}`);
+    });
 }
 
 
-function load() {
+async function load() {
     //渲染页面 因为添加了之后 ul里会一直有li 所以每次再渲染页面的时候 要清空li
     dothings.innerHTML = ''
     donethings.innerHTML = ''
@@ -47,31 +54,28 @@ function load() {
     let docount = 0
     let donecount = 0
     //获取本地储存里的内容 现在是对象形式
-    let data = getData()
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].done) {
-            //如果里面的done属性的值是true 创建li
+    let datas = await getData()
+    console.log(datas)
+    for (let i = 0; i < datas.length; i++) {
+        if (datas[i].attributes.done) {
             let lis = document.createElement("li")
-            lis.innerHTML = "<span class='front'></span><input type='checkbox' checked='checked' class='rightbox'  id=" + i + "><p class='word' id=" + i + ">" + data[i].title + "</p><span class='close' id=" + i + "></span>"
+            lis.innerHTML = "<span class='front'></span><input type='checkbox' checked='checked' class='rightbox'  id=" + datas[i].id + "><p class='word' id=" + datas[i].id + ">" + datas[i].attributes.title + "</p><span class='close' id=" + datas[i].id + "></span>"
             donethings.appendChild(lis)
-            //数量++
             donecount++
 
         } else {
             let lis = document.createElement("li")
-            lis.innerHTML = "<span class='front'></span><input type='checkbox' id=" + i + " class='rightbox'><p class='word' id=" + i + ">" + data[i].title + "</p><span class='close' id=" + i + " ></span>"
+            lis.innerHTML = "<span class='front'></span><input type='checkbox' id=" + datas[i].id + " class='rightbox'><p class='word' id=" + datas[i].id + ">" + datas[i].attributes.title + "</p><span class='close' id=" + datas[i].id + " ></span>"
             dothings.appendChild(lis)
             docount++
         }
     }
-    //每次点击关闭 或者 点击box 和修改p里面的内容都要重新保存数据 和渲染页面
-    //因为每次li里面的各个内容都是动态生成的 所以在load外面没法获取 只能在load里面 每次渲染添加之后在=再重新获取元素
+
     let closes = document.querySelectorAll(".close")
     for (let i = 0; i < closes.length; i++) {
-        closes[i].addEventListener("click", function () {
-            let index = this.id
-            data.splice(index, 1)
-            saveData(data)
+        closes[i].addEventListener("click", async function () {
+            const todo = AV.Object.createWithoutData('Todolist', this.id);
+            await todo.destroy()
             load()
         })
     }
@@ -79,13 +83,15 @@ function load() {
     //点击之后checked变成checked 保存数据 再渲染页面 修改任务出现的位置
     let inputs = document.querySelectorAll(".rightbox")
     for (let i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener("click", function () {
-            let index = this.id
-            data[index].done = this.checked
-            saveData(data)
+        inputs[i].addEventListener("click", async function () {
+            console.log(inputs[i].id)
+            const todo = AV.Object.createWithoutData('Todolist', inputs[i].id);
+            await todo.set('done', inputs[i].checked)
+            todo.save()
             load()
         })
     }
+
     //获取p标签
     let ps = document.querySelectorAll(".word")
     for (let i = 0; i < ps.length; i++) {
@@ -101,18 +107,10 @@ function load() {
             pinputs.addEventListener("blur", function () {
                 let index = ps[i].id
                 //修改data对应数据的内容 再重新渲染页面 input自动消失 创建的p里面没有input
-                data[index].title = this.value
-                saveData(data)
+                datas[index].title = this.value
+                saveData(datas)
                 load()
             })
-            // pinputs.addEventListener("keydown", event => {
-            //     if (event.keyCode === 13) {
-            //         let index = ps[i].id
-            //         data[index].title = pinputs.value
-            //         saveData(data)
-            //         load()
-            //     }
-            // })
         })
     }
     //修改任务数量
