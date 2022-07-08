e2c99e25665b79c010a67ca02ab6ee00af0e012a
renyongwen <ryw0313@qq.com>
2022-07-08 17:45:31 +0800

    diary

--- js/diary.js
+++ js/diary.js
@@ -22,16 +22,6 @@ $('#image').on('change', async function () {
 });
 
 
-toggle.addEventListener("click", async event => {
-    $("#write").fadeToggle();
-    if (write.getAttribute('style').length === 11) {
-        toggle.innerHTML = "写日记";
-    } else {
-        toggle.innerHTML = "看日记";
-    }
-    await load()
-})
-
 submit.addEventListener("click", async event => {
     if (content.value !== '') {
         saveData({title: title.value, content: content.value})
@@ -90,9 +80,9 @@ function saveData(data) {
     diary.set('weather', weather());
     diary.set('time', time());
     diary.set('image', file);
-    if(returnCitySN['cname'][0]==="天"){
+    if (returnCitySN['cname'][0] === "天") {
         diary.set('author', "小燃");
-    }else if(returnCitySN['cname'][0]==="云"){
+    } else if (returnCitySN['cname'][0] === "云") {
         diary.set('author', "梦竹");
     }
     diary.save();
@@ -112,14 +102,14 @@ async function load() {
             olddate = newdate;
         }
         let avatar = 'img/users/avatar-1.jpg'
-        if(datas[i].attributes.author==="小燃"){
+        if (datas[i].attributes.author === "小燃") {
             avatar = 'img/users/xiaoran.png';
-        }else if(datas[i].attributes.author==="梦竹"){
+        } else if (datas[i].attributes.author === "梦竹") {
             avatar = 'img/users/mengzhu.png';
         }
         let lis = document.createElement("li")
         lis.innerHTML =
-            "<img class=\"tl-circ\" src="+avatar+"></img>\n" +
+            "<img class=\"tl-circ\" src=" + avatar + "></img>\n" +
             "<div class=\"timeline-panel\">\n" +
             "<div class=\"tl-heading\">\n" +
             "<h4>" + datas[i].attributes.title + "</h4>\n" +
@@ -133,7 +123,7 @@ async function load() {
             "</div>";
         if (datas[i].attributes.image) {
             lis.innerHTML =
-                "<img class=\"tl-circ\" src="+avatar+"></img>\n" +
+                "<img class=\"tl-circ\" src=" + avatar + "></img>\n" +
                 "<div class=\"timeline-panel\">\n" +
                 "<div class=\"tl-heading\">\n" +
                 "<h4>" + datas[i].attributes.title + "</h4>\n" +
@@ -224,3 +214,73 @@ function html5Reader(file, pic, addImg, deleteImg) {
     addImg.hide();
     deleteImg.show();
 }
+
+var oDiv = document.getElementById('write');
+var span = document.getElementById('writer');
+var a = 0;
+//定义变量a，点击开启或关闭后a加一，用奇偶数的变换来判断是改开还是改关。
+span.onclick = function () {
+    if (a % 2 === 0) {
+        open();
+        a++;
+    } else {
+        close();
+        a++;
+    }
+}
+var timer = null;
+
+function open() {
+    oDiv.hidden = true
+}
+
+function close() {
+    oDiv.hidden = false
+}
+
+
+span.addEventListener('mousedown', function (e) {
+    var x = e.pageX - span.offsetLeft;
+    var y = e.pageY - span.offsetTop;
+    //鼠标移动，触发移动事件
+    document.addEventListener('mousemove', move)
+
+    function move(e) {
+        span.style.left = e.pageX - x + 'px';
+        span.style.top = e.pageY - y + 'px';
+        if(e.pageX-x<0){
+            span.style.left='0px';
+        }
+        if(e.pageX-x>span.parentElement.offsetWidth-span.offsetWidth){
+            span.style.left=span.parentElement.offsetWidth-span.offsetWidth+'px';
+        console.log(span.parentElement.offsetWidth-span.offsetWidth)
+        }
+                if(e.pageY-y<0){
+            span.style.top='0px';
+        }
+        if(e.pageY-y>span.parentElement.offsetHeight-span.offsetHeight-50){
+            span.style.top=span.parentElement.offsetHeight-span.offsetHeight-50+'px';
+        console.log(span.parentElement.offsetWidth-span.offsetWidth-50)
+        }
+    }
+
+    //鼠标抬起，销毁鼠标移动事件
+    document.addEventListener('mouseup', function () {
+        document.removeEventListener('mousemove', move);
+    })
+})
+
+oDiv.addEventListener('mousedown', function (e) {
+    var y = e.pageY - oDiv.offsetTop;
+    //鼠标移动，触发移动事件
+    document.addEventListener('mousemove', move)
+
+    function move(e) {
+        oDiv.style.top = e.pageY - y + 'px';
+    }
+
+    //鼠标抬起，销毁鼠标移动事件
+    document.addEventListener('mouseup', function () {
+        document.removeEventListener('mousemove', move);
+    })
+})
